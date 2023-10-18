5f6089c5e095ebba2a47d13978b7ff7d3c983b8c
Ismail-Lh <ismailsaad03@gmail.com>
2023-10-18 19:13:26 +0100

    reverse string problem solution

---
+++ data_structures/Arrays/reverse_str.js
@@ -0,0 +1,46 @@
+function reverse_str_1(str) {
+  if (!str || str.length < 2 || typeof str !== "string") return false;
+
+  const rev_str = [];
+
+  let last_index = str.length - 1;
+
+  for (let i = 0; i < str.length; i++) {
+    rev_str.push(str[last_index]);
+
+    last_index--;
+  }
+
+  return rev_str.join("");
+}
+
+function reverse_str_2(str) {
+  if (!str || str.length < 2 || typeof str !== "string") return false;
+
+  const rev_str = [];
+
+  const total_chars = str.length;
+
+  for (let i = total_chars; i >= 0; i--) rev_str.push(str[i]);
+
+  return rev_str.join("");
+}
+
+const reverse_str_3 = (str) => {
+  if (!str || str.length < 2 || typeof str !== "string") return false;
+
+  return str.split("").reverse().join("");
+};
+
+function reverse_str_4(str) {
+  let reversed = "";
+  for (let i = str.length - 1; i >= 0; i--) {
+    reversed += str[i];
+  }
+  return reversed;
+}
+
+console.log(reverse_str_1("hello world"));
+console.log(reverse_str_2("hello world"));
+console.log(reverse_str_3("hello world"));
+console.log(reverse_str_4("hello world"));
